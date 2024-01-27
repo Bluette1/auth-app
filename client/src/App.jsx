e95a31744278e83364c7e207c121dc61a5f422c8
Bluette1 <marylene.sawyer@gmail.com>
2024-01-27 20:15:45 +0200

    Add Register form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,36 +7,70 @@ function App() {
   const [showSignUp, setShowSignUp] = useState(false);
 
   const register = () => {
+    const bodyEl = document.querySelector("body");
+    const registerBtn = document.querySelector(".register-btn");
+
+    bodyEl.classList.add("absolute-bg");
+    registerBtn.classList.add("hide");
+
     setShowSignUp(true);
   };
 
   return (
     <>
       <body>
-        <article className="img-logo">
-        <img className="logo" src={logo} alt="logo" />
+      <article className="img-logo">
+            <img className="logo" src={logo} alt="logo" />
+          </article>{" "}
+        <section className="logo-area">
+          
+          <h1>Secure your profile</h1>
+        </section>
 
-        </article>
         <main className="">
           <>
             <article className="">
-              <h1>Secure your profile</h1>
-              <button onClick={register}>Register</button>
+              <button className="register-btn" onClick={register}>
+                Register
+              </button>
             </article>
           </>
         </main>
-        
-      </body>
-      <></>
 
-      <footer>
-        <p className="attribution">
-          Design by{" "}
-          <a href="https://portfolio-ms-app.onrender.com/" target="_blank">
-            Top Weaver
-          </a>
-        </p>
-      </footer>
+        {showSignUp && (
+          <section className="signup">
+            <h3 >Register</h3>
+            <form action="">
+              <label htmlFor="username">
+                <p> Username:</p>
+                <input type="text" name="username" id="" />
+              </label>
+
+              <label htmlFor="email">
+                <p>Email:</p>
+                <input type="email" name="email" id="" />
+              </label>
+
+              <label htmlFor="password">
+                <p>Password:</p>
+
+                <input type="password" name="password" id="" />
+              </label>
+
+              <button type="submit">Submit</button>
+            </form>
+          </section>
+        )}
+
+        <footer>
+          <p className="attribution">
+            Design by{" "}
+            <a href="https://portfolio-ms-app.onrender.com/" target="_blank">
+              Top Weaver
+            </a>
+          </p>
+        </footer>
+      </body>
     </>
   );
 }
