ee058d9c5c2ce852fbe0adc642e5fb634e9c3983
Bluette1 <marylene.sawyer@gmail.com>
2024-02-02 09:53:37 +0200

    Add username check to middleware

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const UsersController = require('../controllers/users');
 const checkJwt  = require('../middleware/check-jwt');
+const checkUsername  = require('../middleware/check-username');
+
 
 router.get('/:id', [checkJwt], UsersController.getOneById);
-router.post('/', [], UsersController.newUser);
+router.post('/', [checkUsername], UsersController.newUser);
 
 
 module.exports = router;
